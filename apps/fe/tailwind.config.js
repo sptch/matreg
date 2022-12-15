0ddfc03f6f2e0e6a41b545d22c9fee61178cef1b
Axel Nilsson <33802893+Axeln78@users.noreply.github.com>
2022-12-15 18:24:11 +0100

    Tailwind integration for the UI library

--- apps/fe/tailwind.config.js
+++ apps/fe/tailwind.config.js
@@ -1,6 +1,8 @@
 const { createGlobPatternsForDependencies } = require('@nrwl/react/tailwind');
 const { join } = require('path');
 
+const colors = require('tailwindcss/colors')
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
@@ -11,7 +13,27 @@ module.exports = {
     ...createGlobPatternsForDependencies(__dirname),
   ],
   theme: {
+    colors: {
+      transparent: 'transparent',
+      current: 'currentColor',
+      'ocean-blue': '#0E7490',
+      'ocean-green': '#43978F',
+      'tangerine-yellow': '#FDBA74',
+      'salmon': '#FECACA',
+      'seaweed-green': '#14B8A6',
+      'silver': '#ecebff',
+      'bubble-gum': '#ff77e9',
+      'bermuda': '#78dcca',
+      black: colors.black,
+      white: colors.white,
+      gray: colors.gray,
+      emerald: colors.emerald,
+      indigo: colors.indigo,
+      yellow: colors.yellow,
+    },
     extend: {},
   },
-  plugins: [],
+  plugins: [
+    require('@tailwindcss/forms'),
+  ],
 };
